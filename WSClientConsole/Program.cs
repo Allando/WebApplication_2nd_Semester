03113cf1873ed5a9635657eb0cf89ab0a19f7db0
Allando <alla2634@edu.easj.dk>
2016-08-16 19:39:37 +0200

    Make something better

--- WSClientConsole/Program.cs
+++ WSClientConsole/Program.cs
@@ -3,20 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Serialization;
 using WebApplication_2nd_Semester;
 
 namespace WSClientConsole
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Select whacha want to do!");
+            Console.WriteLine("1. See all hotels\n2. See Specific hotel");
+
+            var choice = Console.ReadLine();
+            while (choice != "0")
+            {
+                if (choice == "1" || choice == "2")
+                {
+                    switch (choice)
+                    {
+                        case "1":
+                            ListHotels(TODO, TODO2);
+                            break;
+                        case "2":
+                            ShowSpecificHotel(TODO, TODO2, TODO);
+                            break;
+                    }
+                }
+            }
             const string serverUrl = "http://localhost:6015";
             HttpClientHandler handler = new HttpClientHandler();
             handler.UseDefaultCredentials = true;
 
+        }
+        private static void ListHotels(HttpMessageHandler handler, string serverUrl)
+        {
+            //HTTP GET All
             using (var client = new HttpClient(handler))
             {
                 client.BaseAddress = new Uri(serverUrl);
@@ -29,15 +51,56 @@ namespace WSClientConsole
                     if (responce.IsSuccessStatusCode)
                     {
                         var hotels = responce.Content.ReadAsAsync<IEnumerable<Hotel>>().Result;
-                        foreach (var h in hotels)
+
+                        var hotelQuery =
+                            from h in hotels
+                            select h;
+
+                        foreach (var h in hotelQuery)
                         {
                             Console.WriteLine(h);
                         }
                     }
                 }
-                catch
+                catch (ArgumentException e)
                 {
-                    
+                    Console.WriteLine("pao0jdk" + e);
+                }
+            }
+        }
+
+        private static void ShowSpecificHotel(HttpMessageHandler handler, string serverUrl, object Hotel_No)
+        {
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    var response = client.GetAsync("api/hotel/" + Hotel_No).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+
+                        var hotels = response.Content.ReadAsAsync<IEnumerable<Hotel>>().Result;
+
+                        var hotelQuery =
+                            from h in hotels
+                            
+                            select h;
+
+                        foreach (var h in hotelQuery)
+                        {
+                            Console.WriteLine(h);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
                 }
             }
         }
