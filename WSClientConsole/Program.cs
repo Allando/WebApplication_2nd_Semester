4fc46080f67ae31d57beef395965f947fe984499
Allando <alla2634@edu.easj.dk>
2016-08-16 14:23:25 +0200

    Get hotels

--- WSClientConsole/Program.cs
+++ WSClientConsole/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using WebApplication_2nd_Semester;
 
 namespace WSClientConsole
 {
@@ -10,6 +13,34 @@ namespace WSClientConsole
     {
         static void Main(string[] args)
         {
+            const string serverUrl = "http://localhost:6015";
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jason"));
+
+                try
+                {
+                    var responce = client.GetAsync("api/hotels").Result;
+                    if (responce.IsSuccessStatusCode)
+                    {
+                        var hotels = responce.Content.ReadAsAsync<IEnumerable<Hotel>>().Result;
+                        foreach (var h in hotels)
+                        {
+                            Console.WriteLine(h);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    
+                    throw;
+                }
+            }
         }
     }
 }
