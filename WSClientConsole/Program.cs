3d4cd05b71a5b9eca77fb1989410fc359f25a5f9
Allando <alla2634@edu.easj.dk>
2016-08-18 15:32:33 +0200

    Post complete

--- WSClientConsole/Program.cs
+++ WSClientConsole/Program.cs
@@ -16,72 +16,88 @@ namespace WSClientConsole
             HttpClientHandler handler = new HttpClientHandler();
             handler.UseDefaultCredentials = true;
 
-            //HTTP GET All
-            using (var client = new HttpClient(handler))
-            {
-                client.BaseAddress = new Uri(serverUrl);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                try
-                {
-                    var response = client.GetAsync("api/hotels").Result;
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var hotels = response.Content.ReadAsAsync<IEnumerable<Hotel>>().Result;
-
-                        var hotelQuery =
-                            from h in hotels
-                            select h;
-
-                        foreach (var h in hotelQuery)
-                        {
-                            Console.WriteLine("HELLO" + h);
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("GET ALL ERROR!!" + e);
-                    Console.ReadLine();
-                }
-            }  
-
-
-
+            ////HTTP GET All
             //using (var client = new HttpClient(handler))
             //{
             //    client.BaseAddress = new Uri(serverUrl);
             //    client.DefaultRequestHeaders.Clear();
             //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
             //    try
             //    {
-            //        var response = client.GetAsync("api/hotels/5").Result;
-
+            //        var response = client.GetAsync("api/hotels").Result;
             //        if (response.IsSuccessStatusCode)
             //        {
+            //            var hotels = response.Content.ReadAsAsync<IEnumerable<Hotel>>().Result;
 
-            //            var hotels = response.Content.ReadAsAsync<Hotel>().Result;
+            //            var hotelQuery =
+            //                from h in hotels
+            //                select h;
 
-            //            //var hotelQuery =
-            //            //    from h in hotels
-            //            //    where h.Hotel_No == 5
-            //            //    select h;
-
-            //            foreach (var h in hotels)
+            //            foreach (var h in hotelQuery)
             //            {
-            //                Console.WriteLine(h);
+            //                Console.WriteLine("HELLO" + h);
             //            }
+            //        }
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine("GET ALL ERROR!!" + e);
+            //        Console.ReadLine();
+            //    }
+            //}
+
+            ////GET ONE HOTEL
+            //using (var client = new HttpClient(handler))
+            //{
+            //    client.BaseAddress = new Uri(serverUrl);
+            //    client.DefaultRequestHeaders.Clear();
+            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //    string urlString = "api/hotels/5";
+            //    try
+            //    {
+            //        var response = client.GetAsync(urlString).Result;
+            //        if (response.IsSuccessStatusCode)
+            //        {
+            //            var hotels = response.Content.ReadAsAsync<Hotel>().Result;
+            //            Console.WriteLine(hotels);
             //        }
             //    }
             //    catch (ArgumentException e)
             //    {
-
             //        Console.WriteLine("GET 1 ERROR!!" + e);
             //    }
             //}
 
-            Console.ReadLine();
+            
+            //Hotel til db
+            var ShitHotel = new Hotel()
+            {
+                Address = "GangstaStreet 420",
+                Name = "Hotel Motel"
+            };
+            
+            //POST Hotel
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    var response = client.PostAsJsonAsync<Hotel>("api/hotels", ShitHotel).Result;
+
+                    Console.WriteLine("Hotellet er oprettet");
+                    Console.WriteLine("Statuskode: " + response.StatusCode);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Der skete en fejl...");
+                    Console.WriteLine(e); 
+                }
+            }
+
+
         }
     }
 }
